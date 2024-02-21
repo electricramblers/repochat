f80d540d3bc92c80d3a998a64389712c4022281a
Mike Fischer <mike@theelectricrambler.com>
2024-02-21 17:58:22 +0100

    double error check

--- repochat/configmaker.py
+++ repochat/configmaker.py
@@ -13,46 +13,49 @@ from repochat.constants import (
 
 def create_yaml_file():
     filename = absolute_path_to_config()
-    # Define the data structure for the YAML content
-    data = {
-        "blocked_file_paths": ["example/path/1", "example/path/2"],
-        "allowed_file_extensions": [".py", ".html", ".css", ".js", ".md"],
-        "blocked_files": [
-            "__init__.py",
-            "README.md",
-            ".gitignore",
-            ".DS_Store",
-            "example.sublime-project",
-            "example.sublime-workpace",
-        ],
-        "github": {
-            "url": "< your github url >",
-            "token": "<your token here>",
-            "branch": "main",
-            "username": "<your github user name>",
-        },
-        "models": {
-            "escalation": True,
-            "ollama": {
-                "local": "dolphin-mistral:v2.6",
-                "remote": "dolphin-mistral:v2.6",
-                "base_url": "<network ollama like this http://ip-address:11434>",
+    if os.path.exists(absolute_path_to_config()):
+        # Define the data structure for the YAML content
+        data = {
+            "blocked_file_paths": ["example/path/1", "example/path/2"],
+            "allowed_file_extensions": [".py", ".html", ".css", ".js", ".md"],
+            "blocked_files": [
+                "__init__.py",
+                "README.md",
+                ".gitignore",
+                ".DS_Store",
+                "example.sublime-project",
+                "example.sublime-workpace",
+            ],
+            "github": {
+                "url": "< your github url >",
+                "token": "<your token here>",
+                "branch": "main",
+                "username": "<your github user name>",
             },
-            "openrouter": {
-                "low": "openchat/openchat-7b:free",
-                "medium": "google/gemini-pro",
-                "high": "mistralai/mistral-medium",
-                "supervisor": "openai/gpt-4-turbo-preview",
+            "models": {
+                "escalation": True,
+                "ollama": {
+                    "local": "dolphin-mistral:v2.6",
+                    "remote": "dolphin-mistral:v2.6",
+                    "base_url": "<network ollama like this http://ip-address:11434>",
+                },
+                "openrouter": {
+                    "low": "openchat/openchat-7b:free",
+                    "medium": "google/gemini-pro",
+                    "high": "mistralai/mistral-medium",
+                    "supervisor": "openai/gpt-4-turbo-preview",
+                },
+                "embedding": {
+                    "huggingface": {
+                        "model": "sentence-transformers/all-mpnet-base-v2",
+                        "use": True,
+                    }
+                },
             },
-            "embedding": {
-                "huggingface": {
-                    "model": "sentence-transformers/all-mpnet-base-v2",
-                    "use": True,
-                }
-            },
-        },
-        "keys": {"openrouter": "<your api-key goes here>"},
-    }
+            "keys": {"openrouter": "<your api-key goes here>"},
+        }
 
-    with open(os.path.join(filename), "w") as file:
-        yaml.dump(data, file, default_flow_style=False)
+        with open(os.path.join(filename), "w") as file:
+            yaml.dump(data, file, default_flow_style=False)
+    else:
+        pass
